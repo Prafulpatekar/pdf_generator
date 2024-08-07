bb10710591a50095058e3330eeffd7be0ec4866f
Praful Patekar <prafulpatekar23@gmail.com>
2024-08-07 13:51:40 +0530

    Removing Secrets

--- src/pdf_generator/configs/settings.py
+++ src/pdf_generator/configs/settings.py
@@ -8,9 +8,5 @@ POSTGRES_PORT = "5432"
 DATABASE_NAME = "pdf_generator"
 POSTGRES_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_USER_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{DATABASE_NAME}"
 
-# EMAIL settings
-AUTHOUR_EMAIL = "prafulpatekar.dev@gmail.com"
-EMAIL_PASSWORD = "thhz kazn zfgl tmqx" # app password for gmail
-
 # PDF settings
 PDF_TEMPLATE_PATH = "sample_pdf.pdf"
